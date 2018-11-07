e168766f81139bf1bbca81d6fdc7f484cd887ec2
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-07 19:09:32 +0100

    Add fitness to see how well our genes match target phrase (number of matched characters divided by target length)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const target = 'pie is a lie';
 
 let population = [];
-let totalPopulation = 200;
+let totalPopulation = 500;
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,12 +15,26 @@ function getRandomChar() {
 class Genotype {
     constructor(targetLen) {
         this.genes = []; // To store phrase
+        this.fitness = 0; // To see how well it matches target
         for (let i = 0; i < targetLen; i++) {
             this.genes[i] = getRandomChar();
         }
     }
+
+    calculateFitness(target) {
+        let score = 0;
+        for (let i = 0; i < this.genes.length; i++) {
+            if (this.genes[i] === target.charAt(i)) {
+                score++;
+            }
+        }
+        this.fitness = score / target.length;
+    }
+
 }
 
 for (let i = 0; i < totalPopulation; i++) {
     population[i] = new Genotype(target.length);
+    population[i].calculateFitness(target);
+    console.log(population[i]);
 }
